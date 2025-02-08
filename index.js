505d0215adb5ff4e95d74b9c417c0547117ed78e
pankajmores <ommore212005@gmail.com>
2025-02-08 23:16:38 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "pankajmores",
+  "id": 136971374,
+  "node_id": "U_kgDOCCoEbg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/136971374?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/pankajmores",
+  "html_url": "https://github.com/pankajmores",
+  "followers_url": "https://api.github.com/users/pankajmores/followers",
+  "following_url": "https://api.github.com/users/pankajmores/following{/other_user}",
+  "gists_url": "https://api.github.com/users/pankajmores/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/pankajmores/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/pankajmores/subscriptions",
+  "organizations_url": "https://api.github.com/users/pankajmores/orgs",
+  "repos_url": "https://api.github.com/users/pankajmores/repos",
+  "events_url": "https://api.github.com/users/pankajmores/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/pankajmores/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 3,
+  "following": 3,
+  "created_at": "2023-06-18T11:32:18Z",
+  "updated_at": "2025-02-08T16:49:13Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +53,10 @@ app.get('/login', (req,res) => {
 
 app.get('/github', (req,res) => {
     res.send('<h2>pankajmores</h2>')
+})
+
+app.get('/mithub', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
